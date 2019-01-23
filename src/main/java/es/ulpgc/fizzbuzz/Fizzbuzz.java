27407feffce3960aa2a68911fa09f8313b7cf829
sergio <sergiogarciavera1997@gmail.com>
2019-01-23 10:31:47 +0000

    more tests passed

--- src/main/java/es/ulpgc/fizzbuzz/Fizzbuzz.java
+++ src/main/java/es/ulpgc/fizzbuzz/Fizzbuzz.java
@@ -1,4 +1,21 @@
 package es.ulpgc.fizzbuzz;
 
 public class Fizzbuzz {
+    public Fizzbuzz(){
+
+    }
+
+    public String get(int i) {
+        if(isMultipleOfThree(i)) return "Fizz";
+        if(isMultipleOfFive(i)) return "Buzz";
+        return Integer.toString(i);
+    }
+
+    private boolean isMultipleOfFive(int i) {
+        return i % 5 == 0;
+    }
+
+    private boolean isMultipleOfThree(int i) {
+        return i % 3 == 0;
+    }
 }
--- src/test/java/es/ulpgc/fizzbuzz/Fizzbuzz_.java
+++ src/test/java/es/ulpgc/fizzbuzz/Fizzbuzz_.java
@@ -1,4 +1,38 @@
 package es.ulpgc.fizzbuzz;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class Fizzbuzz_ {
+    Fizzbuzz f;
+    @Before
+    public void setUp() throws Exception {
+        f = new Fizzbuzz();
+    }
+    @Test
+    public void given_1_should_return_1() {
+        assertThat(f.get(1)).isEqualTo("1");
+    }
+
+    @Test
+    public void given_2_should_return_2() {
+        assertThat(f.get(2)).isEqualTo("2");
+    }
+
+    @Test
+    public void given_3_should_return_Fizz() {
+        assertThat(f.get(3)).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void given_5_should_return_Buzz() {
+        assertThat(f.get(5)).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void given_6_should_return_Fizz() {
+        assertThat(f.get(6)).isEqualTo("Fizz");
+    }
 }
